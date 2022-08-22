40239802bd98f21854ba643a75682ca8cb6bde20
Sean Dunn <sean@attamusc.com>
2022-08-22 13:55:02 -0400

    feat(wezterm): switch to visual bell for alarms

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,15 @@
 local wezterm = require("wezterm")
 
 return {
+  audible_bell = "Disabled",
+  visual_bell = {
+    fade_in_duration_ms = 75,
+    fade_out_duration_ms = 75,
+    target = 'CursorColor',
+  },
   use_fancy_tab_bar = false,
   color_scheme = "carbonfox",
-  font = wezterm.font("Monolisa NF", {
+  font = wezterm.font("MonoLisa NF", {
     weight = "Light"
   }),
   font_size = 16.0
