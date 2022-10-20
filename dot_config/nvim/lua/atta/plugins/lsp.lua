08c7e6d432a4edaea11d3e93ed44dc9a66633b69
Sean Dunn <attamusc@github.com>
2022-10-20 08:26:12 -0400

    feat: Switch to catppuccin theme

--- dot_config/nvim/lua/atta/plugins/lsp.lua
+++ dot_config/nvim/lua/atta/plugins/lsp.lua
@@ -258,13 +258,42 @@ local function setup_kind()
 end
 
 local function setup_null_ls()
+	local eslint_config = {
+		condition = function(h)
+			return h.root_has_file({
+				"eslint.config.js",
+				".eslintrc",
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+				".eslintrc.json",
+			})
+		end,
+	}
+
 	null_ls.setup({
 		on_attach = on_attach,
 		sources = {
 			-- js
-			null_ls.builtins.formatting.prettier,
-			null_ls.builtins.diagnostics.eslint_d,
-			null_ls.builtins.code_actions.eslint_d,
+			null_ls.builtins.formatting.prettier.with({
+				condition = function(h)
+					return h.root_has_file({
+						".prettierrc",
+						".prettierrc.json",
+						".prettierrc.yml",
+						".prettierrc.yaml",
+						".prettierrc.json5",
+						".prettierrc.js",
+						".prettierrc.cjs",
+						".prettierrc.toml",
+						"prettier.config.js",
+						"prettier.config.cjs",
+					})
+				end,
+			}),
+			null_ls.builtins.diagnostics.eslint_d.with(eslint_config),
+			null_ls.builtins.code_actions.eslint_d.with(eslint_config),
 
 			-- python
 			null_ls.builtins.formatting.black,
