98579650d4e8962713191e587193bb98812a4254
Sean Dunn <attamusc@github.com>
2022-12-09 15:44:52 -0500

    feat(nvim): Add config for vscode-neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,19 @@
-local plugins = require("atta.plugins")
 local main = require("atta.main")
 
-plugins.setup_packer()
-main.setup()
-plugins.setup_plugins()
+if not vim.g.vscode then
+	local plugins = require("atta.plugins")
+
+	plugins.setup_packer()
+	main.setup()
+	plugins.setup_plugins()
+else
+	local vscode = require("atta.vscode")
+
+	-- General settings and keybinding
+	main.setup()
+
+	-- VSCode specific settings used by vscode-neovim
+	-- NOTE: Some of these settings override general settings based on limitations
+	-- of the way vscode-neovim works.
+	vscode.setup()
+end
