309b616ce7b0de976a21b9f7530f147d14489a19
Sean Dunn <attamusc@github.com>
2023-01-19 09:36:37 -0500

    chore(nvim): breaking plugin changes

--- dot_config/nvim/lua/atta/plugins/lspsaga.lua
+++ dot_config/nvim/lua/atta/plugins/lspsaga.lua
@@ -1,19 +1,38 @@
 local M = {
 	"glepnir/lspsaga.nvim",
+	event = "BufRead",
 	branch = "main",
 }
 
 function M.config()
 	local saga = require("lspsaga")
+	local utils = require("atta.utils")
+	local cmd = vim.cmd
 
-	saga.init_lsp_saga({
-		code_action_lightbulb = {
+	saga.setup({
+		ui = {
+			title = false,
+		},
+		lightbulb = {
 			virtual_text = false,
 		},
 		symbol_in_winbar = {
 			enable = false,
 		},
 	})
+	--
+	-- hover docs
+	utils.noremap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+
+	-- code action
+	utils.noremap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+
+	-- diagnostics
+	utils.noremap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+	utils.noremap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+	utils.noremap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+
+	cmd([[autocmd CursorHold * Lspsaga show_cursor_diagnostics]])
 end
 
 return M
