0fb00281a63d32aac76b266c9868b455f18097b9
Sean Dunn <sean@attamusc.com>
2023-02-05 10:58:25 -0500

    nvim: Switch to comment.nvim

--- dot_config/nvim/lua/atta/plugins/nerdcommenter.lua
+++ dot_config/nvim/lua/atta/plugins/nerdcommenter.lua
@@ -1,18 +1,12 @@
 local g = vim.g
 
 local M = {
-	"preservim/nerdcommenter",
+	"numToStr/Comment.nvim",
 }
 
 function M.config()
-	local utils = require("atta.utils")
-
-	g.NERDCreateDefaultMappings = 0
-	g.NERDSpaceDelims = 1
-	g.NERDToggleCheckAllLines = 1
-
-	utils.map("", "<leader>c<space>", "<plug>NERDCommenterToggle")
-	utils.map("", "<leader>cc", "<plug>NERDCommenterComment")
+	local comment = require("Comment")
+	comment.setup()
 end
 
 return M
