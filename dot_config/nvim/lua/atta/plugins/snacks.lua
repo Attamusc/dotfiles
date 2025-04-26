eac82844b2468e2d9fb25ac759806c682f571bc4
Sean Dunn <sean@attamusc.com>
2025-04-26 16:35:12 -0400

    fix: snacks dashboard keys nesting

--- dot_config/nvim/lua/atta/plugins/snacks.lua
+++ dot_config/nvim/lua/atta/plugins/snacks.lua
@@ -7,14 +7,32 @@ return {
 		bigfile = { enabled = true },
 		dashboard = {
 			enabled = true,
-			keys = {
-				{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-				{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-				{ icon = " ", key = "c", desc = "Chat", action = ":CodeCompanionChat Toggle" },
-				{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-				{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+			preset = {
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{ icon = " ", key = "e", desc = "Explorer", action = ":lua require('oil').open_float()" },
+					{ icon = " ", key = "c", desc = "Chat", action = ":CodeCompanionChat Toggle" },
+					{
+						icon = "󰒲 ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
 			},
 			sections = {
 				{ section = "header" },
