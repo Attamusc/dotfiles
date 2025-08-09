8b1b06a2601fb64883f86b6661c1f82d9dcd37bf
Sean Dunn <attamusc@github.com>
2025-08-09 11:02:01 -0400

    feat: Improve the display of the battery status in sketchybar

--- dot_config/sketchybar/plugins/executable_battery.sh
+++ dot_config/sketchybar/plugins/executable_battery.sh
@@ -1,40 +1,45 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 source "$CONFIG_DIR/colors.sh"
 
-PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
-CHARGING="$(pmset -g batt | grep 'AC Power')"
+BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(pmset -g batt | grep 'AC Power')
 
-if [ "$PERCENTAGE" = "" ]; then
+sketchybar --set "${NAME}" icon.color=0xff989898
+
+if [[ ${CHARGING} != "" ]]; then
+  case ${BATT_PERCENT} in
+  100) ICON="󱐋" COLOR="$GREEN" ;;
+  9[0-9]) ICON="󱐋" COLOR="$GREEN" ;;
+  8[0-9]) ICON="󱐋" COLOR="$GREEN" ;;
+  7[0-9]) ICON="󱐋" COLOR="$GREEN" ;;
+  6[0-9]) ICON="󱐋" COLOR="$YELLOW" ;;
+  5[0-9]) ICON="󱐋" COLOR="$YELLOW" ;;
+  4[0-9]) ICON="󱐋" COLOR="$ORANGE" ;;
+  3[0-9]) ICON="󱐋" COLOR="$ORANGE" ;;
+  2[0-9]) ICON="󱐋" COLOR="$RED" ;;
+  1[0-9]) ICON="󱐋" COLOR="$RED" ;;
+  *) ICON="󱐋" COLOR="$RED" ;;
+  esac
+
+  sketchybar --set "${NAME}" icon="${ICON}" icon.color="${COLOR}"
+  sketchybar --set "${NAME}" label="${BATT_PERCENT}%"
   exit 0
 fi
 
-case "${PERCENTAGE}" in
-9[0-9] | 100)
-  ICON=""
-  COLOR=$GREY
-  ;;
-[6-8][0-9])
-  ICON=""
-  COLOR=$GREY
-  ;;
-[3-5][0-9])
-  ICON=""
-  COLOR=$YELLOW
-  ;;
-[1-2][0-9])
-  ICON=""
-  COLOR=$ORANGE
-  ;;
-*)
-  ICON=""
-  COLOR=$RED
-  ;;
+case ${BATT_PERCENT} in
+100) ICON="" COLOR="$GREEN" ;;
+9[0-9]) ICON="" COLOR="$GREEN" ;;
+8[0-9]) ICON="" COLOR="$GREEN" ;;
+7[0-9]) ICON="" COLOR="$GREEN" ;;
+6[0-9]) ICON="" COLOR="$YELLOW" ;;
+5[0-9]) ICON="" COLOR="$YELLOW" ;;
+4[0-9]) ICON="" COLOR="$ORANGE" ;;
+3[0-9]) ICON="" COLOR="$ORANGE" ;;
+2[0-9]) ICON="" COLOR="$RED" ;;
+1[0-9]) ICON="" COLOR="$RED" ;;
+*) ICON="" COLOR="$RED" ;;
 esac
 
-if [[ "$CHARGING" != "" ]]; then
-  ICON="󱐋"
-  COLOR=$GREY
-fi
-
-sketchybar --set "$NAME" icon="$ICON" icon.color="$COLOR" label="${PERCENTAGE}%" label.color="$COLOR"
+sketchybar --set "${NAME}" icon="${ICON}" icon.color="${COLOR}"
+sketchybar --set "${NAME}" label="${BATT_PERCENT}%"
